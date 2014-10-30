246a5b8820af2aec30981b4cc37e7f1efc68bfe4
Marchal yoan <marchalyoan@gmail.com>
2014-10-30 13:29:50 +0100

    remove versionning

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,15 +56,6 @@ module.exports = function(grunt) {
         }
       }
     },
-    version: {
-      options: {
-        file: 'lib/scripts.php',
-        css: 'assets/css/main.min.css',
-        cssHandle: 'roots_main',
-        js: 'assets/js/scripts.min.js',
-        jsHandle: 'roots_scripts'
-      }
-    },
     watch: {
       less: {
         files: [
