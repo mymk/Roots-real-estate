398a73674068d52206f35a4470555c4dd593002f
Marchal yoan <marchalyoan@gmail.com>
2014-10-30 16:32:18 +0100

    remove partners

--- templates/partenaires.php
+++ templates/partenaires.php
@@ -1,20 +1,3 @@
 <?php 
 /* page partenaires */
 ?>
-	<!-- Section: partners -->
-	<section class="row double-bottom">
-
-		<h2 class="col-sm-12"><?php _e('Ours partenaires', 'roots-immo'); ?></h2>
-
-		<ul class="partners nav nav-pills nav-justified col-sm-12 add-bottom add-top">
-			<li><a href="#"><img src="http://dummyimage.com/104x53/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image"></a></li>
-			<li><a href="#"><img src="http://dummyimage.com/104x53/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image"></a></li>
-			<li><a href="#"><img src="http://dummyimage.com/104x53/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image"></a></li>
-			<li><a href="#"><img src="http://dummyimage.com/104x53/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image"></a></li>
-			<li><a href="#"><img src="http://dummyimage.com/104x53/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image"></a></li>
-			<li><a href="#"><img src="http://dummyimage.com/104x53/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image"></a></li>
-			<li><a href="#"><img src="http://dummyimage.com/104x53/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image"></a></li>
-		</ul>
-
-	</section>
-	<!-- /section: partners -->
