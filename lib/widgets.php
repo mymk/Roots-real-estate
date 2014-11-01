34da3306ea6d9aa8024ed38063429dddd05d846a
Marchal yoan <marchalyoan@gmail.com>
2014-11-01 14:08:22 +0100

    undo last changes

--- lib/widgets.php
+++ lib/widgets.php
@@ -7,10 +7,10 @@ function roots_widgets_init() {
   register_sidebar(array(
     'name'          => __('Primary', 'roots'),
     'id'            => 'sidebar-primary',
-    'before_widget' => '<div class="panel panel-default widget %1$s %2$s">',
-    'after_widget'  => '</div></div>',
-    'before_title'  => '<div class="panel-heading"><h3 class="panel-title">',
-    'after_title'   => '</h3></div><div class="panel-body">',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3 >',
+    'after_title'   => '</h3>',
   ));
 
   register_sidebar(array(
